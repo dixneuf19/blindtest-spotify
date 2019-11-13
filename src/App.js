c7e9e08f98130840be2c1004d2151f08da566bbd
Julen Dixneuf <julen@loudnaround.org>
2019-11-13 18:38:58 +0100

    use spotify api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+// Not secure, but you can only read my library with this token
+const apiToken = `BQCaG8sdCndUjvnQLzGbPkUrqorRLhAY3mI43xjGskqy353zr7NgRrYAjiiJXNZ2eQ0vVvVo-ROkUj6kFv3fwHE_ClFVyeS950hdsmWw5F5G7pBEj07lWGhHWmvB-6hyb75bGWy7wjrq9Rc1mxop6uDt`;
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -32,17 +33,38 @@ class App extends Component {
 
   constructor() {
     super();
+    this.state = {
+      text: ""
+    }
+  }
+
+  async componentDidMount() {
+    this.setState({
+      text: "Bonjour"
+    })
+
+    const res = await fetch('https://api.spotify.com/v1/me/tracks', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + apiToken,
+      },
+    })
+      .then(response => response.json())
+
+    this.setState({
+      text: `Your library has ${res.total} songs !`
+    })
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Bienvenue sur le Blindtest</h1>
         </header>
         <div className="App-images">
-          <p>Work in progress</p>
+          <p>{this.state.text}</p>
         </div>
         <div className="App-buttons">
         </div>
